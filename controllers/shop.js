b1f09b5269c1ba95b78dec58707b16af66a58281
hkharshita <komraharshita@gmail.com>
2024-08-15 23:49:38 +0530

    first commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart=require('../models/cart');
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -39,12 +39,22 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart=(req,res,next)=>{
+const prodId=req.body.productId;
+Product.findById(prodId,(product)=>{
+Cart.addProduct(prodId,product.price);
+})
+res.redirect('/cart');
+}
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
     pageTitle: 'Your Orders'
   });
 };
+
+
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
